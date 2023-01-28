f8511ae1162ae2af5ad82aa9fe55ad3963ffeba9
Falgun <77203052+fxlgun@users.noreply.github.com>
2023-01-28 13:23:10 +0530

    added getmemes method

--- utils/twitter.js
+++ utils/twitter.js
@@ -2,6 +2,43 @@ import axios from 'axios'
 
 export default class Twitter {
     constructor(twitterAuthToken) {
+        this.getMemes = async (word) => {
+            const queryText = word + ' #meme'
+            const headers = {
+                Authorization: twitterAuthToken,
+            }
+            const images = []
+            return await axios
+                .get(
+                    `https://api.twitter.com/2/tweets/search/recent?max_results=100&media.fields=url,media_key&expansions=attachments.media_keys&query=elon %23meme`,
+                    { headers }
+                )
+                .then((response) => {
+                    const tweets = response.data.includes["media"];
+                    tweets.forEach(element => {
+                        if (element['type'] === 'photo') {
+                            images.push(element['url'])
+                        }
+                        
+                    });
+                    console.log(images);
+                    // tweets.forEach((tweet) => {
+                    //     if (tweet.entities && tweet.entities.media) {
+                    //         tweet.entities.media.forEach(function (image) {
+                    //             console.log(image.media_url);
+                    //             images.push(image.media_url)
+                    //         })
+                    //     }
+                    //     return images;
+                    // })
+                    return images
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            
+        }
+
         this.getTweets = async (word, length) => {
             const headers = {
                 Authorization: twitterAuthToken,
